b3c878111126e90e0050e7a3bab22a5dfd57292e
pawan.t@cisinlabs.com <pawan.t@cisinlabs.com>
2022-05-09 20:46:47 +0530

    add decommerce,rinkeby network config

--- packages/dapp/src/hooks/useBridgeDirection.js
+++ packages/dapp/src/hooks/useBridgeDirection.js
@@ -6,77 +6,74 @@ import { networks } from 'lib/networks';
 import { useCallback, useMemo } from 'react';
 
 export const useBridgeDirection = () => {
-  const { bridgeDirection } = useSettings();
-  const bridgeConfig = useMemo(
-    () => networks[bridgeDirection] || Object.values(networks)[0],
-    [bridgeDirection],
-  );
+    const { bridgeDirection } = useSettings();
+    const bridgeConfig = useMemo(
+        () => networks[bridgeDirection] || Object.values(networks)[0], [bridgeDirection],
+    );
 
-  const {
-    homeChainId,
-    foreignChainId,
-    ambLiveMonitorPrefix,
-    homeGraphName,
-    foreignGraphName,
-    homeAmbAddress,
-    foreignAmbAddress,
-  } = bridgeConfig;
+    const {
+        homeChainId,
+        foreignChainId,
+        ambLiveMonitorPrefix,
+        homeGraphName,
+        foreignGraphName,
+        homeAmbAddress,
+        foreignAmbAddress,
+    } = bridgeConfig;
 
-  const foreignAmbVersion = useAmbVersion(foreignChainId, foreignAmbAddress);
+    const foreignAmbVersion = useAmbVersion(foreignChainId, foreignAmbAddress);
 
-  const homeRequiredSignatures = useRequiredSignatures(
-    homeChainId,
-    homeAmbAddress,
-  );
+    const homeRequiredSignatures = useRequiredSignatures(
+        homeChainId,
+        homeAmbAddress,
+    );
 
-  const { homeTotalConfirms, foreignTotalConfirms } = useTotalConfirms(
-    homeChainId,
-    foreignChainId,
-    homeAmbAddress,
-    foreignAmbAddress,
-  );
+    const { homeTotalConfirms, foreignTotalConfirms } = useTotalConfirms(
+        homeChainId,
+        foreignChainId,
+        homeAmbAddress,
+        foreignAmbAddress,
+    );
 
-  const getBridgeChainId = useCallback(
-    chainId => (chainId === homeChainId ? foreignChainId : homeChainId),
-    [homeChainId, foreignChainId],
-  );
+    const getBridgeChainId = useCallback(
+        chainId => (chainId === homeChainId ? foreignChainId : homeChainId), [homeChainId, foreignChainId],
+    );
 
-  const getMonitorUrl = useCallback(
-    (chainId, hash) => `${ambLiveMonitorPrefix}/${chainId}/${hash}`,
-    [ambLiveMonitorPrefix],
-  );
+    const getMonitorUrl = useCallback(
+        (chainId, hash) => `${ambLiveMonitorPrefix}/${chainId}/${hash}`, [ambLiveMonitorPrefix],
+    );
 
-  const getGraphEndpoint = useCallback(
-    chainId => {
-      const subgraphName =
-        homeChainId === chainId ? homeGraphName : foreignGraphName;
-      return `https://api.thegraph.com/subgraphs/name/${subgraphName}`;
-    },
-    [foreignGraphName, homeChainId, homeGraphName],
-  );
+    const getGraphEndpoint = useCallback(
+        chainId => {
+            const subgraphName =
+                homeChainId === chainId ? homeGraphName : foreignGraphName;
+            if (homeChainId === 8787 || homeChainId === 4) {
+                return subgraphName
+            }
+            return `https://api.thegraph.com/subgraphs/name/${subgraphName}`;
+        }, [foreignGraphName, homeChainId, homeGraphName],
+    );
 
-  const getAMBAddress = useCallback(
-    chainId => (chainId === homeChainId ? homeAmbAddress : foreignAmbAddress),
-    [homeChainId, homeAmbAddress, foreignAmbAddress],
-  );
+    const getAMBAddress = useCallback(
+        chainId => (chainId === homeChainId ? homeAmbAddress : foreignAmbAddress), [homeChainId, homeAmbAddress, foreignAmbAddress],
+    );
 
-  const getTotalConfirms = useCallback(
-    chainId =>
-      chainId === homeChainId ? homeTotalConfirms : foreignTotalConfirms,
-    [homeChainId, homeTotalConfirms, foreignTotalConfirms],
-  );
+    const getTotalConfirms = useCallback(
+        chainId =>
+        chainId === homeChainId ? homeTotalConfirms : foreignTotalConfirms, [homeChainId, homeTotalConfirms, foreignTotalConfirms],
+    );
 
-  return {
-    bridgeDirection,
-    getBridgeChainId,
-    getMonitorUrl,
-    getGraphEndpoint,
-    getAMBAddress,
-    foreignAmbVersion,
-    homeRequiredSignatures,
-    homeTotalConfirms,
-    foreignTotalConfirms,
-    getTotalConfirms,
-    ...bridgeConfig,
-  };
+    return {
+        bridgeDirection,
+        getBridgeChainId,
+        getMonitorUrl,
+        getGraphEndpoint,
+        getAMBAddress,
+        foreignAmbVersion,
+        homeRequiredSignatures,
+        homeTotalConfirms,
+        foreignTotalConfirms,
+        getTotalConfirms,
+        ...bridgeConfig,
+    };
 };
